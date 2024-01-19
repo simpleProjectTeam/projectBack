ba33436d57c1991322a5030b0590ba37ca61e829
wooyoung0407 <wooyoung0407@naver.com>
2024-01-19 19:43:47 +0900

    mysql 임시get작업

--- app.js
+++ app.js
@@ -1,28 +1,61 @@
-// app.js
 import express from "express";
 import cors from "cors";
+import mysql from "mysql2/promise";
 
 const app = express();
-app.use(cors());
+const port = 3000;
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "project",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  dateStrings: true,
+});
+
 
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:3000",
   optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get("/:user_code/account", async (req, res) => {
+  const { user_code } = req.params;
 
-app.get("/about", (req, res) => {
-  res.send("About!");
-});
+  try {
+    const [prices] = await pool.query(
+      `
+      SELECT *
+      FROM accountBook
+      WHERE user_code = ?
+      ORDER BY id ASC
+      `,
+      [user_code]
+    );
 
-app.get("/setting", (req, res) => {
-  res.send("Setting!");
+    const formattedPrices = prices.map((item) => ({
+      ...item,
+      price: `${Number(item.price).toLocaleString()}`,
+    }));
+
+    res.json({
+      resultCode: "S-1",
+      msg: "성공",
+      data: formattedPrices,
+    });
+  } catch (error) {
+    console.error("Error fetching data from accountBook:", error);
+    res.status(500).json({
+      resultCode: "F-1",
+      msg: "서버 오류",
+    });
+  }
 });
 
 app.listen(port, () => {
