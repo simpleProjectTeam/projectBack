220a5a52e7e33ad91697cdfdb5aaa945f2ac42bf
wooyoung0407 <wooyoung0407@naver.com>
2024-01-20 13:35:58 +0900

    단건조회

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const pool = mysql.createPool({
   dateStrings: true,
 });
 
-
 const corsOptions = {
   origin: "http://localhost:3000",
   optionsSuccessStatus: 200,
@@ -25,6 +24,15 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+
+const formatPrices = (prices) => {
+  return prices.map((item) => ({
+    ...item,
+    price: `${Number(item.price).toLocaleString()}`,
+  })); //소수점 제거를 위한 공용
+};
+
+// 조회
 app.get("/:user_code/account", async (req, res) => {
   const { user_code } = req.params;
 
@@ -39,20 +47,58 @@ app.get("/:user_code/account", async (req, res) => {
       [user_code]
     );
 
-    const formattedPrices = prices.map((item) => ({
-      ...item,
-      price: `${Number(item.price).toLocaleString()}`,
-    }));
+    const formattedPrices = formatPrices(prices);
 
     res.json({
       resultCode: "S-1",
       msg: "성공",
       data: formattedPrices,
     });
+
   } catch (error) {
-    console.error("Error fetching data from accountBook:", error);
+    console.error("데이터베이스 쿼리 실행 중 오류:", error);
     res.status(500).json({
       resultCode: "F-1",
+      msg: "서버 오류",
+    });
+  }
+});
+
+// 단건조회
+app.get("/:user_code/account/:no", async (req, res) => {
+  const { user_code, no } = req.params;
+
+  try {
+    const [prices] = await pool.query(
+      `
+      SELECT *
+      FROM accountBook
+      WHERE user_code = ?
+      AND no = ?
+      `,
+      [user_code, no]
+    );
+
+    const formattedPrices = formatPrices(prices);
+
+    if (!formattedPrices.length) {
+      res.status(404).json({
+        resultCode: "F-1",
+        msg: "실패",
+      });
+      return;
+    }
+
+    res.json({
+      resultCode: "S-1",
+      msg: "성공",
+      data: formattedPrices,
+    });
+
+  } catch (error) {
+    console.error("데이터베이스 쿼리 실행 중 오류:", error);
+    res.status(500).json({
+      resultCode: "F-2",
       msg: "서버 오류",
     });
   }
